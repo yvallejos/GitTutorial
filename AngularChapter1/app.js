fe67fcbbfa5305c956277b9b780afe5fce0d66a6
Yasiell Vallejos <yvallejos@penixlabs.com>
2016-05-20 17:21:44 -0600

    Ejercicios del nivel 2 de Angular-CodeSchool

--- AngularChapter1/app.js
+++ AngularChapter1/app.js
@@ -10,12 +10,22 @@
 			price: 2.95,
 			description: 'This is a beautiful gem',
 			canPurchase: true,
+			images: [
+				{
+					full: '1.jpg_256',
+				}
+			]
 		},
 		{
 			name: 'Pentagonal Gem',
 			price: 5.95,
 			description: 'This is a beautiful gem',
 			canPurchase: false,
+			images: [
+				{
+					full: '2.jpg_256',
+				}
+			]
 		}
 	];
 })();
