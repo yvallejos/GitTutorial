117c610c37b9e5b2a6212e909435dbc889d82531
Yasiell Vallejos <yvallejos@penixlabs.com>
2016-05-23 16:51:42 -0600

    Se termino el tutorial de angular con fallas

--- AngularTutorial/app.js
+++ AngularTutorial/app.js
@@ -1,67 +1,23 @@
 (function(){
-	var app = angular.module('store',[]);
-	app.controller('StoreController',function(){
-		this.products = gems;
-	});
+	var app = angular.module('store',['store-products']);
+	app.controller('StoreController',['$http',function($http){
+		var store = this;
+    store.products = [ ];
+    $http.get('/products.json').success(function(data){
+      store.products = data;
+    });
+	}]);
 
-  app.controller('PanelController',function(){
-    this.tab = 1;
+  
 
-    this.selectTab = function(setTab){
-      this.tab = setTab;
-    };
+  app.controller('ReviewController',function(){
+    this.review = {};
 
-    this.isSelected = function(checkTab){
-      return this.tab === checkTab;
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
     };
   });
 
-	var gems = [
-		{
-			name: 'Dodecahedron',
-			price: 2.95,
-			description: 'This is a beautiful gem',
-			canPurchase: true,
-			images: [
-				{
-					full: '1.jpg_256',
-				}
-			],
-      reviews: [ 
-        {
-          stars: 5,
-          body: "I love this product",
-          author: "joe@thomas.com"
-        },
-        {
-          stars: 1,
-          body: "this product is ugly",
-          author: "joe@hater.com"
-        }
-      ]
-		},
-		{
-			name: 'Pentagonal Gem',
-			price: 5.95,
-			description: 'This is a beautiful gem',
-			canPurchase: false,
-			images: [
-				{
-					full: '2.jpg_256',
-				}
-			],
-      reviews: [ 
-        {
-          stars: 4,
-          body: "Is beautiful",
-          author: "teo@thomas.com"
-        },
-        {
-          stars: 1,
-          body: "this product is too expensive",
-          author: "teo@hater.com"
-        }
-      ]
-		}
-	];
+  
 })();
